7788617ad9b88f0b090f40b8bcb7958dc1aec792
kcthota <kcthota@gmail.com>
2016-01-16 16:13:20 -0800

    HSL2RGB conversion

--- colorme.js
+++ colorme.js
@@ -3,29 +3,29 @@
     var colorMe = {};
     
     colorMe.HexColor = function (hex) {
-        if (hex) {
-            var sc_parts, parts = [], val = hex.toString();
+        var sc_parts, parts = [], val = hex.toString();
 
-            //check for shortcode - #123 should be #112233
-            sc_parts = /^#?([a-f\d])([a-f\d])([a-f\d])$/i.exec(val);
+        //check for shortcode - #123 should be #112233
+        sc_parts = /^#?([a-f\d])([a-f\d])([a-f\d])$/i.exec(val);
 
-            if (sc_parts) {
-                sc_parts.splice(1).forEach(function (val) {
-                    parts.push(val + val);
-                });
-            } else {
-                parts = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
-                parts = parts.splice(1);
-            }
-
-            if (parts) {
-                this.r_part = parts[0];
-                this.g_part = parts[1];
-                this.b_part = parts[2];
-            }
+        if (sc_parts) {
+            sc_parts.splice(1).forEach(function (val) {
+                parts.push(val + val);
+            });
+        } else {
+            parts = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
+            parts = parts.splice(1);
         }
-        
-        return null;
+
+        if (parts) {
+            this.r_part = parts[0];
+            this.g_part = parts[1];
+            this.b_part = parts[2];
+        } else {
+            this.r_part = '00';
+            this.g_part = '00';
+            this.b_part = '00';
+        }
     };
     
     colorMe.HexColor.prototype = {
@@ -88,15 +88,12 @@
                 h *= 60;
             }
             
-            s = parseFloat(s).toFixed(2);
-            l = parseFloat(l).toFixed(2);
-            
             return new colorMe.HSL(h, s, l);
         }
     };
     
     colorMe.HSL = function (h, s, l) {
-        var h_val = parseFloat(h).toFixed(0) || 0,
+        var h_val = Math.round(h) || 0,
             s_val = parseFloat(s) || 0,
             l_val = parseFloat(l) || 0;
         
@@ -108,7 +105,58 @@
     
     colorMe.HSL.prototype = {
         getRGB: function () {
+            var r, g, b, t1, t2, h_val, r1, g1, b1,
+                hue2rgb = function (t1, t2, h) {
+                    var color;
+                    if (6 * h < 1) {
+                        color = t2 + (t1 - t2) * 6 * h;
+                    } else if (2 * h < 1) {
+                        color = t1;
+                    } else if (3 * h < 2) {
+                        color = t2 + (t1 - t2) * (0.666 - h) * 6;
+                    } else {
+                        color = t2;
+                    }
+                    return color;
+                },
+                correctColor = function (color) {
+                    var val = color;
+                    if (color > 1) {
+                        val = color - 1;
+                    } else if (color < 0) {
+                        val = color + 1;
+                    }
+                    
+                    if (!(val > 0 && val < 1)) {
+                        val = correctColor(val);
+                    }
+                    return val;
+                };
             
+            if (this.s === 0) {
+                r = g = b = this.l * 255;
+            } else {
+                if (this.l < 0.5) {
+                    t1 = this.l * (1.0 + this.s);
+                } else {
+                    t1 = this.l + this.s - this.l * this.s;
+                }
+                
+                t2 = 2 * this.l - t1;
+                h_val = this.h / 360;
+                
+                r1 = correctColor(h_val + 0.333);
+                g1 = correctColor(h_val);
+                b1 = correctColor(h_val - 0.333);
+                
+                r = Math.round(hue2rgb(t1, t2, r1) * 255);
+                g = Math.round(hue2rgb(t1, t2, g1) * 255);
+                b = Math.round(hue2rgb(t1, t2, b1) * 255);
+            }
+            return new colorMe.RGB(r, g, b);
+        },
+        getHex: function () {
+            return this.getRGB().getHex();
         }
     };
     

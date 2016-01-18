40ff21deeb741bcfbbe135c0f7820b0e0d5d9451
kcthota <kcthota@gmail.com>
2016-01-18 00:04:44 -0800

    default gulp task and license

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
-gulp.task('default', function () {
-
-});
+gulp.task('default', ['test', 'compress']);
 
 gulp.task('compress', function () {
   return gulp.src('lib/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+	.pipe(uglify({
+        preserveComments: 'license'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('test', function() {

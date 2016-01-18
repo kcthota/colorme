7c355f5c35c9bef64dae779edbb37fa16f659dfc
kcthota <kcthota@gmail.com>
2016-01-18 00:51:43 -0800

    linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
-gulp.task('default', ['test', 'compress']);
+gulp.task('default', ['lint', 'test', 'compress']);
+
+gulp.task('travis', ['lint', 'test']);
 
 gulp.task('compress', function () {
   return gulp.src('lib/*.js')
 	.pipe(uglify({
         preserveComments: 'license'
+    }))
+    .pipe(rename({
+      extname: '.min.js'
     }))
     .pipe(gulp.dest('dist'));
 });
@@ -16,4 +23,11 @@ gulp.task('test', function() {
   return gulp.src(['lib/*.js', 'spec/*spec.js'])
     .pipe(jasmineBrowser.specRunner({console: true}))
     .pipe(jasmineBrowser.headless());
+});
+
+gulp.task('lint', function() {
+  return gulp.src('lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
